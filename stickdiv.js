acf26d97744c051d8a19fc2f7fa337285e7a3e66
Yohanes Oktavianus <yohanes.lumentut@yahoo.com>
2015-09-23 18:34:26 +0700

    2nd Commit

--- stickdiv.js
+++ stickdiv.js
@@ -1,54 +1,33 @@
 ( function(  $, window ){
 
-    $.fn.fixTopHeader = function( options ) {
-        var defaults = $.extend({
-            header:'.panel-heading',
-            bottom:'.panel-footer',
-            active:'active',
-            widthLimit: 475,
-            fromBottom: 70
-        }, options);
+    $.fn.stickyDiv = function() {
 
-        var el = $(this);
+        var el = $( this );
+        var elWidth = el.parent().width();
+        var stickyPos = el.position();
+        var windWidth = $( window).width();
+        var limit = 977;
 
-        if(el.find(defaults.bottom).length === 1)
-        {
-            var _hdr = el.find(defaults.header);
-            var _ftr = el.find(defaults.bottom);
-            var _elPos = el.position();
-            var _hdrPos = _hdr.position();
-            var _ftrPos = _ftr.position();
+        $( window ).resize(function() {
+            stickyPos = el.position();
+            elWidth = el.parent().width();
+            windWidth = $( this ).width();
+            el.width( elWidth );
+        });
 
-            $(window).resize(function() {
-                _elPos = el.position();
-                _hdrPos = _hdr.position();
-                _ftrPos = _ftr.position();
+        $( window ).scroll(function() {
 
-                var windowWidth = $(this).width();
+            var windowPos = $( this ).scrollTop();
 
-                if ( windowWidth > defaults.widthLimit ) {
-                    _hdr.removeClass(defaults.active);
-                }
-            });
+            if ( windowPos >= stickyPos.top && windWidth >= limit ) {
+                    el.addClass( 'active' );
+                    el.width( elWidth );
+            } else {
+                el.removeClass( 'active' );
+            }
+        });
 
-            $(window).scroll(function() {
-                var windowPos = $(this).scrollTop();
-                var windowWidth = $(this).width();
-
-                if ( windowWidth < defaults.widthLimit ) {
-                    if (windowPos >= (_ftrPos.top - defaults.fromBottom)) {
-                        _hdr.removeClass(defaults.active);
-                    } else if (windowPos < _elPos.top) {
-                        _hdr.removeClass(defaults.active);
-                    } else if (windowPos >= _hdrPos.top) {
-                        _hdr.addClass(defaults.active);
-                    } else {
-                        _hdr.removeClass(defaults.active);
-                    }
-                }
-            });
-            return this;
-        }
+        return this;
     };
 
 }( jQuery, window ));
